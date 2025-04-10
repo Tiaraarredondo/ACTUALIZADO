5f8afccef4af3e8f7fd6a8c7a44c2908a5b81e75
sofiagarziaa <sgarzia@udesa.edu.ar>
2025-04-10 17:28:32 -0300

    Navegacion

--- proyect-ti/src/App.js
+++ proyect-ti/src/App.js
@@ -1,13 +1,23 @@
 //aqui es donde generamos la estructura de la app y tambiene es el componente principal
 import React from "react";
 import Footer from './components/Footer/Footer';
+import {Switch, Route } from 'react-router-dom'
+import Home from "./screens/Home/Home";
+// import Detalle from "./screens/Detalle/Detalle";
+// import NotFound from "./screens/NotFound/NotFound";
 import Header from './components/Header/Header';
+import Navegacion from './components/Navegacion/Navegacion';
 
 function App() {
   return (
     <>
+    <Navegacion/>
     <Header/>
-    <p>hola esto</p>
+        <Switch>
+        <Route path={'/'} exact={true} component={Home} />
+        {/* <Route path={'/detalle/:id'} component={Detalle} /> */}
+        {/* <Route path={''} component={NotFound} /> */}
+      </Switch>
     {/* JSX: extension de Js la cual produce elementos html */}
     <Footer />
     </>
