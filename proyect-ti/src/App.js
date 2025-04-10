3f86f633ae5b7425473f08054788642d4ca70bd0
sofiagarziaa <sgarzia@udesa.edu.ar>
2025-04-10 00:26:48 -0300

    creacion de carpetas(components)

--- proyect-ti/src/App.js
+++ proyect-ti/src/App.js
@@ -1,7 +1,8 @@
+//aqui es donde generamos la estructura de la app y tambiene es el componente principal
+import React from "react";
 function App() {
   return (
-<p>hola esto lo hice yo</p>
+    <p>React</p>     //JSX: extension de Js la cual produce elementos html
   );
 }
-
 export default App;
