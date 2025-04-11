398d61ee11593db2f234087488a61539af0dbb21
sofiagarziaa <sgarzia@udesa.edu.ar>
2025-04-11 14:49:51 -0300

    5 elementos de populares y 5 de de cartelera

--- proyect-ti/src/components/Peliculas/Peliculas.js
+++ proyect-ti/src/components/Peliculas/Peliculas.js
@@ -6,8 +6,7 @@ function Peliculas({ data }) {
             <h3>{data.title}</h3>
             <h4>Descripcion:</h4>
             <h4>{data.overview}</h4>
-            <h4>Esta pelicula fue realizada el {data.release_date}, con ya {data.popularity} de espectadores desde entonces.</h4>
-            <h4>Idioma Original:{data.original_language}</h4>
+        
             {data.poster_path ? (
                 <img
                     src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}
--- proyect-ti/src/screens/Home/Home.js
+++ proyect-ti/src/screens/Home/Home.js
@@ -1,53 +1,69 @@
-import React, { Component } from "react";
-import FiltroPeliculas from "../../components/Peliculas/FiltroPeliculas/FiltroPeliculas";
-import PeliculasCard from "../../components/Peliculas/Peliculas"; 
+import React, { Component } from 'react';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            peliculas: [],
-            backupPeliculas: []
+            populares: [],
+            cartelera: [],
         };
     }
 
     componentDidMount() {
-        const API_KEY = "9f66dc201448c71cc91c3c8c9f488105";
-        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+        const API_KEY = '9f66dc201448c71cc91c3c8c9f488105';
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => this.setState({
-                peliculas: data.results,
-                backupPeliculas: data.results
-            }))
-            .catch(error => console.log(error));
-    }
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+            .then(res => res.json())
+            .then(data => {
+                let cincoPopulares = [];
+                for (let i = 0; i < 5; i++) {
+                    cincoPopulares.push(data.results[i]);
+                }
+                this.setState({ populares: cincoPopulares });
+            })
+            .catch(err => console.log(err));
 
-    filtrarPeliculas = (texto) => {
-        const filtradas = this.state.backupPeliculas.filter(peli =>
-            peli.title.toLowerCase().includes(texto.toLowerCase())
-        );
-        this.setState({ peliculas: filtradas });
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`)
+            .then(res => res.json())
+            .then(data => {
+                let cincoCartelera = [];
+                for (let i = 0; i < 5; i++) {
+                    cincoCartelera.push(data.results[i]);
+                }
+                this.setState({ cartelera: cincoCartelera });
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
-            <>
-                <h1>Todas las Películas</h1>
-                <FiltroPeliculas filtro={this.filtrarPeliculas} />
-                {
-                    this.state.peliculas.length === 0
-                        ? <p>Cargando...</p>
-                        : this.state.peliculas.map((pelicula, idx) => (
-                            <PeliculasCard key={idx} data={pelicula} />
-                        ))
+            <div>
+                <h1>Películas Populares</h1>
+                {this.state.populares.length === 0 ? <p>Cargando...</p> :
+                    <ul>
+                        {this.state.populares.map((peli, i) => (
+                            <li key={i}>
+                                <h3>{peli.title}</h3>
+                                <img src={`https://image.tmdb.org/t/p/w200${peli.poster_path}`} alt={peli.title} />
+                            </li>
+                        ))}
+                    </ul>
                 }
-            </>
+
+                <h1>Películas en Cartelera</h1>
+                {this.state.cartelera.length === 0 ? <p>Cargando...</p> :
+                    <ul>
+                        {this.state.cartelera.map((peli, i) => (
+                            <li key={i}>
+                                <h3>{peli.title}</h3>
+                                <img src={`https://image.tmdb.org/t/p/w200${peli.poster_path}`} alt={peli.title} />
+                            </li>
+                        ))}
+                    </ul>
+                }
+            </div>
         );
     }
 }
 
 export default Home;
-
-
